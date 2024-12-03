b0d6420ef7dfb83218e8b7ace24530cc4136aacf
Nguyen Viet Khoa <khoanv.23bi14223@usth.edu.vn>
2024-12-03 23:06:21 +0700

    First student commit

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,42 +1,29 @@
-"""Student Mark Management System"""
-def display_menu():
-    print("\nStudent Mark Management System")
-    print("1. Input number of students")
-    print("2. Input student information")
-    print("3. Input number of courses")
-    print("4. Input course information")
-    print("5. Select a course and input marks for students")
-    print("6. List courses")
-    print("7. List students")
-    print("8. Show student marks for a given course")
-    print("9. Exit")
-
 # Add students -> class
 def Num_stu(): # Number of students
     try:
         num = int(input("Num of stu/class "))
-        print("Number of student choose: %d" %num)
+        print("Num_stu choose: %d" %num)
         return num
     except ValueError:
         print("Wrong, choose again")
         return Num_stu()
 
 # Add student info
-def Stu_info(students): #Infomation of students
-    student_Id = input("Stu-Id: ")
+def Stu_info(stu): #Infomation of students
+    stu_Id = input("Stu-Id: ")
     name = input("Name: ")
-    dob = input("Date of Birth: ")
-    if student_Id in students:
+    dob = input("DOB: ")
+    if stu_Id in stu:
         print("Exist -> Wrong")
     else:
-        students[student_Id] = {"name": name, "dob": dob, "marks": {}}
-        print("Student %s (Id: %s)" % (name, student_Id))
+        stu[stu_Id] = {"name": name, "dob": dob, "marks": {}}
+        print("Student %s (Id: %s)" % (name, stu_Id))
 
 # Add Num-courses
 def Courses(): #Number of courses
     try:
-        num = int(input("Number of courses: "))
-        print("Number of course choose: %d" % num)
+        num = int(input("Num_cs: "))
+        print("Num_c choose: %d" % num)
         return num
     except ValueError:
         print("Wrong, choose again")
@@ -44,89 +31,101 @@ def Courses(): #Number of courses
 
 # Add course info
 def Course_info(courses): #Infomation of courses
-    course_Id = input("Course Id: ")
-    name = input("Course Name: ")
-    if course_Id in courses:
+    c_Id = input("C-Id: ")
+    name = input("C-Name: ")
+    if c_Id in courses:
         print("Exists -> Wrong")
     else:
-        courses[course_Id] = name
-        print("Course %s (Id: %s)" % (name, course_Id))
+        courses[c_Id] = name
+        print("Course %s (Id: %s)" % (name, c_Id))
 
 # Add marks/course
-def Mark_c(students, courses): # Marks for each courses
-    course_Id = input("Enter Course Id: ")
-    if course_Id not in courses:
-        print("C_Id not found.") # Course
+def Mark_c(stu, courses): # Marks for each courses
+    c_Id = input("C-Id: ")
+    if c_Id not in courses:
+        print("C-Id not found.") # Course
     else:
-        print("Inputting marks for course: %s (Id: %s)" % (courses[course_Id], course_Id))
-        for student_Id, student in students.items():
+        print("Marks: %s (Id: %s)" % (courses[c_Id], c_Id))
+        for stu_Id, stu in stu.items():
             try:
-                mark = float(input("Enter mark for %s (Id: %s): " % (student['name'], student_Id)))
-                student["marks"][course_Id] = mark
-                print(f"Mark for {student['name']} recorded successfully.")
+                mark = float(input("Mark %s (Id: %s): " % (stu['name'], stu_Id)))
+                stu["marks"][c_Id] = mark
+                print("Mark of %s" % stu['name'])
             except ValueError:
                 print("Wrong, choose again")
 
 # List courses
-def list_courses(courses):
+def list_c(courses):
     if not courses:
-        print("Unavailable")
+        print("Unavailable") # not exist
     else:
         print("\nCourses:")
-        for course_Id, name in courses.items():
-            print(f"{course_Id}: {name}")
+        for c_Id, name in courses.items():
+            print("%s: %s" % (c_Id, name))
 
 #List students
-def list_students(students):
-    if not students:
-        print("No students available.")
+def list_stu(stu):
+    if not stu:
+        print("Unavailable.") # not exist
     else:
         print("\nStudents:")
-        for student_Id, data in students.items():
-            print(f"Id: {student_Id}, Name: {data['name']}, DoB: {data['dob']}")
+        for stu_Id, data in stu.items():
+            print("Id: %s, Name: %s, DoB: %s" % (stu_Id, data['name'], data['dob']))
 
-# Student marks/course
-def show_student_marks_for_course(students, courses):
-    course_Id = input("Enter Course Id: ")
-    if course_Id not in courses:
-        print("Course Id not found.")
+# stu-marks/course
+def stu_mark_c(stu, courses):
+    c_Id = input("C-Id: ")
+    if c_Id not in courses:
+        print("Unavailable.") # Course not found
     else:
-        print(f"\nMarks for {courses[course_Id]} (Id: {course_Id}):")
-        for student_Id, student in students.items():
-            mark = student["marks"].get(course_Id, None)
+        print("\nMarks for %s (Id: %s):" % (courses[c_Id], c_Id))
+        for stu_Id, stu in stu.items():
+            mark = stu["marks"].get(c_Id, None)
             if mark is not None:
-                print(f"{student['name']} (Id: {student_Id}): {mark}")
+                print("%s (Id: %s): %s" % (stu['name'], stu_Id, mark))
             else:
-                print(f"{student['name']} (Id: {student_Id}): No mark available.")
+                print("%s (Id: %s): Unavailable." % (stu['name'], stu_Id))
 
-# Main program loop
+# Menu
+def menu():
+    print(""" 
+          1. Number of students:
+          2. Student information: 
+          3. Number of courses:
+          4. Course information: 
+          5. Add marks for students/courses: 
+          6. List courses:
+          7. List students:
+          8. Show student marks/course:
+          9. Exit.""")
+
 def main():
-    students = {}
+    stu = {}
     courses = {}
-    while True:
-        display_menu()
-        choice = input("\nEnter your choice: ")
+    while True: # loop until exit
+        menu()
+        choice = input("\nChoose an option: ")
         if choice == "1":
             Num_stu()
         elif choice == "2":
-            Stu_info(students)
+            Stu_info(stu)
         elif choice == "3":
             Courses()
         elif choice == "4":
             Course_info(courses)
         elif choice == "5":
-            Mark_c(students, courses)
+            Mark_c(stu, courses)
         elif choice == "6":
-            list_courses(courses)
+            list_c(courses)
         elif choice == "7":
-            list_students(students)
+            list_stu(stu)
         elif choice == "8":
-            show_student_marks_for_course(students, courses)
+           stu_mark_c(stu, courses)
         elif choice == "9":
-            print("Exiting the program. Goodbye!")
+            print("Exit.")
             break
         else:
-            print("InvalId choice. Please try again.")
+            print("Wrong, choose again")
 
 if __name__ == "__main__":
     main()
