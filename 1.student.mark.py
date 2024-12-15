bf34aaf084af87c2607a556f14fb278e7751127e
Nguyen Viet Khoa <khoanv.23bi14223@usth.edu.vn>
2024-12-15 23:53:31 +0700

    First student commit

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,132 +1,225 @@
-# SYSTEM of student management
-# Add students -> class
-def Num_stu(): # Number of students
-    try:
-        num = int(input("Num of stu/class "))
-        print("Num_stu choose: %d" %num)
-        return num
-    except ValueError:
-        print("Wrong, choose again")
-        return Num_stu()
+class Person:
+    def __init__(self,name,dob):
+        self.__name = name
+        self.__dob = dob
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_dob(self):
+        return self.__dob
+    
+class Stu(Person):  # Inherit from Person class
+    def __init__(self, id, name, dob, marks={}):
+        super().__init__(name, dob)  # Call the Person's constructor
+        self.__id = id
+        self.__marks = marks
+        
+    def get_id(self):
+        return self.__id
 
-# Add student info
-def Stu_info(stu): #Infomation of students
-    stu_Id = input("Stu-Id: ")
-    name = input("Name: ")
-    dob = input("DOB: ")
-    if stu_Id in stu:
-        print("Exist -> Wrong")
-    else:
-        stu[stu_Id] = {"name": name, "dob": dob, "marks": {}}
-        print("Student %s (Id: %s)" % (name, stu_Id))
+    def get_marks(self):
+        return self.__marks
+    
+    def fill_mark(self, course_id, point):
+        self.__marks[course_id] = point
 
-# Add Num-courses
-def Courses(): #Number of courses
-    try:
-        num = int(input("Num_cs: "))
-        print("Num_c choose: %d" % num)
-        return num
-    except ValueError:
-        print("Wrong, choose again")
-        return Courses()
+    # Display string
+    def __str__(self):
+        return f"StudentID: {self.get_id()}, Name: {self.get_name()}, DoB: {self.get_dob()}"
 
-# Add course info
-def Course_info(courses): #Infomation of courses
-    c_Id = input("C-Id: ")
-    name = input("C-Name: ")
-    if c_Id in courses:
-        print("Exists -> Wrong")
-    else:
-        courses[c_Id] = name
-        print("Course %s (Id: %s)" % (name, c_Id))
+class Course:
+    def __init__(self, id, name):
+        self.__id = id
+        self.__name = name
 
-# Add marks/course
-def Mark_c(stu, courses): # Marks for each courses
-    c_Id = input("C-Id: ")
-    if c_Id not in courses:
-        print("C-Id not found.") # Course
-    else:
-        print("Marks: %s (Id: %s)" % (courses[c_Id], c_Id))
-        for stu_Id, stu in stu.items():
-            try:
-                mark = float(input("Mark %s (Id: %s): " % (stu['name'], stu_Id)))
-                stu["marks"][c_Id] = mark
-                print("Mark of %s" % stu['name'])
-            except ValueError:
-                print("Wrong, choose again")
+    def get_id(self):
+        return self.__id
+    
+    def get_name(self):
+        return self.__name
+    
+    # Display string
+    def __str__(self):
+        return f"CourseID: {self.get_id()}, Name: {self.get_name()}"
 
-# List courses
-def list_c(courses):
-    if not courses:
-        print("Unavailable") # not exist
-    else:
-        print("\nCourses:")
-        for c_Id, name in courses.items():
-            print("%s: %s" % (c_Id, name))
+class Outils:
+    def show(list):
+        for instance in list:
+            print(instance)
 
-#List students
-def list_stu(stu):
-    if not stu:
-        print("Unavailable.") # not exist
-    else:
-        print("\nStudents:")
-        for stu_Id, data in stu.items():
-            print("Id: %s, Name: %s, DoB: %s" % (stu_Id, data['name'], data['dob']))
+    def input_Integer(args) -> int:
+        return int(input(f"Enter the number of {args}: "))
 
-# stu-marks/course
-def stu_mark_c(stu, courses):
-    c_Id = input("C-Id: ")
-    if c_Id not in courses:
-        print("Unavailable.") # Course not found
-    else:
-        print("\nMarks for %s (Id: %s):" % (courses[c_Id], c_Id))
-        for stu_Id, stu in stu.items():
-            mark = stu["marks"].get(c_Id, None)
-            if mark is not None:
-                print("%s (Id: %s): %s" % (stu['name'], stu_Id, mark))
+class School:
+    def __init__(self):
+        self.__num_students = 0
+        self.__num_courses = 0
+        self.__students = []
+        self.__courses = []
+
+    def get_num_students(self):
+        return self.__num_students
+    
+    def get_num_courses(self):
+        return self.__num_courses
+    
+    def get_students(self):
+        return self.__students
+    
+    def get_courses(self):
+        return self.__courses
+    
+    def set_students(self, num):
+        self.__num_students = num
+
+    def set_courses(self, num):
+        self.__num_courses = num    
+
+    def set_num_students(self):
+        self.__num_students = Outils.input_Integer("students")
+
+    def set_num_courses(self):
+        self.__num_courses = Outils.input_Integer("courses")
+
+    def add_student(self):
+        id = input("Enter the student's id: ")
+        name = input("Enter the student's name: ")
+        dob = input("Enter the student's dob: ")  
+        self.__students.append(Stu(id,name,dob))
+    
+    def add_course(self):
+        id = input("Enter the course's id: ")
+        name = input("Enter the course's name: ")
+        self.__courses.append(Course(id,name))
+
+    def input_students(self):
+        if self.get_num_students() == 0:
+            print("Please input the number of students first")  
+        for _ in range(self.get_num_students()):
+            self.add_student()
+
+    def input_courses(self):
+        if self.get_num_courses() == 0:
+            print("Please input the number of courses first")
+        for _ in range(self.get_num_courses()):
+            self.add_course()
+
+    def load_courses(self, courses):
+        self.__courses = courses
+
+    def load_students(self, students):
+        self.__students = students
+
+    def enter_mark(self):
+        course_id = input("Enter the course ID to input marks: ")
+        course = None
+
+        for c in self.__courses:
+            if c.get_id() == course_id:
+                course = c
+                break
+        
+        if course is None:
+            print("Course not found!")
+            return
+        
+        print(f"Entering marks for course: {course.get_name()}")
+        for student in self.__students:
+            mark = float(input(f"Enter marks for student {student.get_name()} (ID: {student.get_id()}): "))
+            student.get_marks()[course_id] = mark
+
+    def display_mark(self):
+        course_id = input("Enter the course ID to view marks: ")
+    
+        course = None
+        for c in self.__courses:
+            if c.get_id() == course_id:
+                course = c
+                break
+
+        if course is None:
+            print("Course not found!")
+            return
+        print(f"Marks for course: {course.get_name()}")
+
+        for student in self.__students:
+            if course_id in student.get_marks():
+                print(f"Student {student.get_name()} (ID: {student.get_id()}): {student.get_marks()[course_id]}")
             else:
-                print("%s (Id: %s): Unavailable." % (stu['name'], stu_Id))
+                print(f"Student {student.get_name()} (ID: {student.get_id()}): No marks entered")
 
-# Menu
-def menu():
-    print(""" 
-          1. Number of students:
-          2. Student information: 
-          3. Number of courses:
-          4. Course information: 
-          5. Add marks for students/courses: 
-          6. List courses:
-          7. List students:
-          8. Show student marks/course:
-          9. Exit.""")
+    def list_students(self):
+        if not self.get_students():
+            print("No students input yet")
+            return
+        
+        print("Student list:")
+        Outils.show(self.get_students())
+
+    def list_courses(self):
+        if not self.get_courses():
+            print("No courses input yet")
+            return
+
+        print("Course list: ")
+        Outils.show(self.get_courses())
 
 def main():
-    stu = {}
-    courses = {}
-    while True: # loop until exit
-        menu()
-        choice = input("\nChoose an option: ")
-        if choice == "1":
-            Num_stu()
-        elif choice == "2":
-            Stu_info(stu)
-        elif choice == "3":
-            Courses()
-        elif choice == "4":
-            Course_info(courses)
-        elif choice == "5":
-            Mark_c(stu, courses)
-        elif choice == "6":
-            list_c(courses)
-        elif choice == "7":
-            list_stu(stu)
-        elif choice == "8":
-           stu_mark_c(stu, courses)
-        elif choice == "9":
-            print("Exit.")
+    Uni = School()
+
+    while True:
+        print("\nOptions:")
+        print("0. Load pre-entered data:(If don't have data, please choose this option first)")
+        print("1. Input number of students:")
+        print("2. Input number of courses:")
+        print("3. Input students info:")
+        print("4. Input courses info:")
+        print("5. Enter mark of students in a specific course:")
+        print("6. Display all students")
+        print("7. Display all courses")
+        print("8. Display mark for a specific course")
+        print("9. Exit\n")   
+
+        choice = int(input("Enter your choice: "))
+        if choice == 0:
+            pre_entered_students: list[Stu] = [
+                Stu("BI14001", "Anh", "15/07/2005", {"ICT1.001": 10.3, "ICT2.004": 3.2}),
+                Stu("BI14002", "Huy", "22/10/2005", {"ICT1.001": 5.0}),
+                Stu("BI14003", "Si", "02/09/2005", {"ICT3.002": -2.0, "ICT1.001": 1.0})
+            ]
+
+            pre_entered_courses: list[Course] = [
+                Course("ICT1.001", "Basic Programming"),
+                Course("ICT2.004", "OOP"),
+                Course("ICT3.002", "Database")
+            ]
+            Uni.set_courses(len(pre_entered_courses))
+            Uni.set_students(len(pre_entered_students))
+            Uni.load_courses(pre_entered_courses)
+            Uni.load_students(pre_entered_students)
+            print("Successfully loaded pre-entered data.")
+        elif choice == 1:
+            Uni.set_num_students()
+        elif choice == 2:
+            Uni.set_num_courses()
+        elif choice == 3:
+            Uni.input_students()
+        elif choice == 4:
+            Uni.input_courses()
+        elif choice == 5:
+            Uni.enter_mark()
+        elif choice == 6:
+            Uni.list_students()
+        elif choice == 7:
+            Uni.list_courses()
+        elif choice == 8:
+            Uni.display_mark()
+        elif choice == 9:
+            print("Exiting the program.")
             break
         else:
-            print("Wrong, choose again")
+            print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
---
+++ 2.student.mark.py
@@ -0,0 +1,211 @@
+class Person:
+    def __init__(self, name, dob):
+        self.__name = name
+        self.__dob = dob
+
+    # Encapsulation: getter methods for private attributes
+    def get_name(self):
+        return self.__name
+
+    def get_dob(self):
+        return self.__dob
+
+    # Polymorphism: Define a common input method
+    def input(self):
+        self.__name = input("Enter name: ")
+        self.__dob = input("Enter Date of Birth (DD/MM/YYYY): ")
+
+    def __str__(self):
+        return f"Name: {self.__name}, DoB: {self.__dob}"
+
+
+class Student(Person):
+    def __init__(self, student_id, name, dob, marks=None):
+        super().__init__(name, dob)  # Call the constructor of Person
+        self.__student_id = student_id
+        self.__marks = marks if marks is not None else {}
+
+    # Encapsulation: getter methods for private attributes
+    def get_student_id(self):
+        return self.__student_id
+
+    def get_marks(self):
+        return self.__marks
+
+    def set_marks(self, course_id, mark):
+        self.__marks[course_id] = mark
+
+    # Polymorphism: Input for student-specific attributes
+    def input(self):
+        super().input()  # Call the input method from Person
+        self.__student_id = input("Enter student ID: ")
+
+    # Display student information
+    def __str__(self):
+        marks_str = ", ".join([f"{course}: {mark}" for course, mark in self.__marks.items()])
+        return f"StudentID: {self.__student_id}, {super().__str__()}, Marks: {marks_str}"
+
+
+class Course:
+    def __init__(self, course_id, course_name):
+        self.__course_id = course_id
+        self.__course_name = course_name
+
+    # Encapsulation: getter methods for private attributes
+    def get_course_id(self):
+        return self.__course_id
+
+    def get_course_name(self):
+        return self.__course_name
+
+    # Polymorphism: Input for course-specific attributes
+    def input(self):
+        self.__course_id = input("Enter course ID: ")
+        self.__course_name = input("Enter course name: ")
+
+    # Display course information
+    def __str__(self):
+        return f"CourseID: {self.__course_id}, Name: {self.__course_name}"
+
+
+class School:
+    def __init__(self):
+        self.__students = []
+        self.__courses = []
+
+    # Encapsulation: Getter methods for private attributes
+    def get_students(self):
+        return self.__students
+
+    def get_courses(self):
+        return self.__courses
+
+    # Methods to add student and course to the lists
+    def add_student(self, student: Student):
+        self.__students.append(student)
+
+    def add_course(self, course: Course):
+        self.__courses.append(course)
+
+    # Polymorphism: method to input student or course data
+    def input(self, entity_type):
+        if entity_type == 'student':
+            student = Student("", "", "", {})
+            student.input()  # Call student-specific input
+            self.add_student(student)
+        elif entity_type == 'course':
+            course = Course("", "")
+            course.input()  # Call course-specific input
+            self.add_course(course)
+
+    # Enter marks for students in a specific course
+    def enter_marks(self):
+        course_id = input("Enter course ID to input marks: ")
+        course = None
+        for c in self.__courses:
+            if c.get_course_id() == course_id:
+                course = c
+                break
+
+        if not course:
+            print("Course not found!")
+            return
+
+        print(f"Entering marks for course: {course.get_course_name()}")
+        for student in self.__students:
+            mark = float(input(f"Enter mark for student {student.get_name()} (ID: {student.get_student_id()}): "))
+            student.set_marks(course_id, mark)
+
+    # Display all students
+    def list_students(self):
+        if not self.__students:
+            print("No students available.")
+        else:
+            for student in self.__students:
+                print(student)
+
+    # Display all courses
+    def list_courses(self):
+        if not self.__courses:
+            print("No courses available.")
+        else:
+            for course in self.__courses:
+                print(course)
+
+    # Display marks for a specific course
+    def display_marks(self):
+        course_id = input("Enter the course ID to display marks: ")
+        course = None
+        for c in self.__courses:
+            if c.get_course_id() == course_id:
+                course = c
+                break
+
+        if not course:
+            print("Course not found!")
+            return
+
+        print(f"Marks for course: {course.get_course_name()}")
+        for student in self.__students:
+            marks = student.get_marks().get(course_id, "No marks entered")
+            print(f"{student.get_name()} (ID: {student.get_student_id()}): {marks}")
+
+
+def main():
+    school = School()
+
+    while True:
+        print("\nOptions:")
+        print("0. Load pre-entered data:(If don't have data, please choose this option first)")
+        print("1. Input number of students:")
+        print("2. Input number of courses:")
+        print("3. Input students info:")
+        print("4. Input courses info:")
+        print("5. Enter marks for a specific course:")
+        print("6. Display all students")
+        print("7. Display all courses")
+        print("8. Display marks for a specific course")
+        print("9. Exit\n")
+
+        choice = int(input("Enter your choice: "))
+        if choice == 0:
+            pre_entered_students = [
+                Student("BI14001", "Anh", "15/07/2005", {"ICT1.001": 10.3, "ICT2.004": 3.2}),
+                Student("BI14002", "Huy", "22/10/2005", {"ICT1.001": 5.0}),
+                Student("BI14003", "Si", "02/09/2005", {"ICT3.002": -2.0, "ICT1.001": 1.0}),
+            ]
+            pre_entered_courses = [
+                Course("ICT1.001", "Basic Programming"),
+                Course("ICT2.004", "OOP"),
+                Course("ICT3.002", "Database"),
+            ]
+            for student in pre_entered_students:
+                school.add_student(student)
+            for course in pre_entered_courses:
+                school.add_course(course)
+            print("Successfully loaded pre-entered data.")
+        elif choice == 1:
+            num_students = int(input("Enter number of students: "))
+            for _ in range(num_students):
+                school.input('student')
+        elif choice == 2:
+            num_courses = int(input("Enter number of courses: "))
+            for _ in range(num_courses):
+                school.input('course')
+        elif choice == 3:
+            school.list_students()
+        elif choice == 4:
+            school.list_courses()
+        elif choice == 5:
+            school.enter_marks()
+        elif choice == 6:
+            school.display_marks()
+        elif choice == 7:
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+
+if __name__ == "__main__":
+    main()
