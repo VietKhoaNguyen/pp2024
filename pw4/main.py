2fec38a9a36cfc35a1db2cb4b3f9ca2322996b1f
Nguyen Viet Khoa <khoanv.23bi14223@usth.edu.vn>
2024-12-19 09:20:01 +0700

    First student commit

--- pw4/main.py
+++ pw4/main.py
@@ -1,21 +1,20 @@
-# pw4/main.py
 import curses
-from pw4.school import School
-from pw4.student import Student
-from pw4.course import Course
+from domains import School
+from input import input_student, input_course
+from output import display_menu
 
 def main(stdscr):
     school = School(stdscr)
 
     pre_entered_students = [
-        Student("BI14001", "Anh", "15/07/2005", {"ICT1.001": 10.3, "ICT2.004": 3.2}),
-        Student("BI14002", "Huy", "22/10/2005", {"ICT1.001": 5.0}),
-        Student("BI14003", "Si", "02/09/2005", {"ICT3.002": -2.0, "ICT1.001": 1.0}),
+        input_student(),
+        input_student(),
+        input_student(),
     ]
     pre_entered_courses = [
-        Course("ICT1.001", "Basic Programming", 3),
-        Course("ICT2.004", "OOP", 4),
-        Course("ICT3.002", "Database", 3),
+        input_course(),
+        input_course(),
+        input_course(),
     ]
 
     for student in pre_entered_students:
@@ -24,8 +23,11 @@ def main(stdscr):
     for course in pre_entered_courses:
         school.add_course(course)
 
-    school.show_ui()
+    while True:
+        display_menu(stdscr)
+        c = stdscr.getch()
+        if c == ord('q'):
+            break
 
 if __name__ == "__main__":
     curses.wrapper(main)
-    
--- pw4/utils.py
+++
@@ -1,7 +0,0 @@
-# pw4/utils.py
-def input_integer(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("Invalid input, please enter a valid integer.")
